672ce1f9858be2bd4b494a37bd17310d65a52c7f
KaworuSSSS <143292307+KaworuSSSS@users.noreply.github.com>
2026-08-06 21:24:18 -0600

    test: add G90 G91 motion planner tests

--- tests/test_motion_planner.py
+++ tests/test_motion_planner.py
@@ -152,3 +152,66 @@ def test_planner_generates_target_positions():
         "Y": 25,
         "Z": 0.0
     }
+    def test_g90_absolute_mode():
+
+    planner = MotionPlanner()
+
+    commands = [
+
+        {
+            "command": "G90"
+        },
+
+        {
+            "command": "G0",
+            "parameters": {
+                "X": 50
+            }
+        },
+
+        {
+            "command": "G0",
+            "parameters": {
+                "X": 60
+            }
+        }
+
+    ]
+
+    result = planner.plan(commands)
+
+    assert result[0]["target"]["X"] == 50
+    assert result[1]["target"]["X"] == 60
+
+
+
+def test_g91_relative_mode():
+
+    planner = MotionPlanner()
+
+    commands = [
+
+        {
+            "command": "G91"
+        },
+
+        {
+            "command": "G0",
+            "parameters": {
+                "X": 50
+            }
+        },
+
+        {
+            "command": "G0",
+            "parameters": {
+                "X": 60
+            }
+        }
+
+    ]
+
+    result = planner.plan(commands)
+
+    assert result[0]["target"]["X"] == 50
+    assert result[1]["target"]["X"] == 110
