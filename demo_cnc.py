7467bf8138f6ba722fca6482745221c7d32d2cfd
KaworuSSSS <143292307+KaworuSSSS@users.noreply.github.com>
2026-08-08 11:13:41 -0600

    Update demo_cnc.py

--- demo_cnc.py
+++ demo_cnc.py
@@ -2,41 +2,49 @@ from app.drivers.cnc_simulator import CNCSimulator
 from app.visualization.cnc_view import CNCView
 
 
-simulator = CNCSimulator()
+def create_demo():
 
-simulator.connect()
+    simulator = CNCSimulator()
 
-simulator.move_absolute(
-    x=10,
-    y=10,
-    z=15,
-)
+    simulator.connect()
 
-simulator.move_absolute(
-    x=80,
-    y=10,
-    z=15,
-)
+    simulator.move_absolute(
+        x=10,
+        y=10,
+        z=15,
+    )
 
-simulator.move_absolute(
-    x=80,
-    y=45,
-    z=15,
-)
+    simulator.move_absolute(
+        x=80,
+        y=10,
+        z=15,
+    )
 
-simulator.move_absolute(
-    x=10,
-    y=45,
-    z=15,
-)
+    simulator.move_absolute(
+        x=80,
+        y=45,
+        z=15,
+    )
 
-simulator.move_absolute(
-    x=10,
-    y=10,
-    z=15,
-)
+    simulator.move_absolute(
+        x=10,
+        y=45,
+        z=15,
+    )
 
+    simulator.move_absolute(
+        x=10,
+        y=10,
+        z=15,
+    )
+
+    return simulator
+
+
+simulator = create_demo()
 
 viewer = CNCView(simulator)
 
-viewer.show()
+fig = viewer.create_figure()
+
+fig.show()
