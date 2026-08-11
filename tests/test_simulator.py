31dce13b1c857e9bf51c18d58126631ccfe80188
KaworuSSSS <143292307+KaworuSSSS@users.noreply.github.com>
2026-08-11 14:33:04 -0600

    Update test_simulator.py

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -28,3 +28,32 @@ def test_move():
     status = cnc.get_status()
 
     assert status["position"]["X"] == 50
+def test_simulator_records_toolpath():
+
+    cnc = CNCSimulator()
+
+    cnc.connect()
+
+    cnc.move_relative("X", 50)
+    cnc.move_relative("Y", 25)
+    cnc.move_relative("Z", -5)
+
+    toolpath = cnc.get_toolpath()
+
+    assert toolpath == [
+        {
+            "X": 50.0,
+            "Y": 0.0,
+            "Z": 0.0
+        },
+        {
+            "X": 50.0,
+            "Y": 25.0,
+            "Z": 0.0
+        },
+        {
+            "X": 50.0,
+            "Y": 25.0,
+            "Z": -5.0
+        }
+    ]
